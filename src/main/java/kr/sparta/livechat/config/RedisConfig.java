b6ec81a2964d770fa1ace6d2b17beaccbc917075
sehyun kim <127721055+shkim6663@users.noreply.github.com>
2025-12-17 20:53:13 +0900

    docs: RedisConfig 클래스 내 Bean 설정 상세 JavaDoc 추가
    - RedisConnectionFactory 및 RedisTemplate 설정 목적 명시
    - 데이터 가독성을 위한 StringRedisSerializer 적용 사유 기술
    
    Co-authored-by: [김세현]
    Refs:#3#4 (GIT/Issue Tracker ID)

--- src/main/java/kr/sparta/livechat/config/RedisConfig.java
+++ src/main/java/kr/sparta/livechat/config/RedisConfig.java
@@ -11,7 +11,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 /**
  * Redis 데이터베이스와의 연결하는 Spring 클래스입니다.
  * yml 파일에서 Redis 정보를 읽어와 RedisConnectionFactory와 RedisTemplate을 생성하여
- * Spring IoC 컨데티어네 등록합니다.
+ * Spring IoC 컨데이너 등록합니다.
  * RedisConfig.java
  *
  * @author kimsehyun
@@ -26,11 +26,22 @@ public class RedisConfig {
 	@Value("${spring.data.redis.port}")
 	private int redisPort;
 
+	/**
+	 * Redis 연결을 위한 ConnectionFactory를 생성합니다.
+	 *
+	 * @return Lettuce 기반의 Redis 연결 팩토리
+	 */
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
 		return new LettuceConnectionFactory(redisHost, redisPort);
 	}
 
+	/**
+	 * 데이터 조작을 위한 RedisTemplate 객체를 생성합니다.
+	 * Redis 저장시 문자열 형태로 지정합니다.
+	 * @param connectionFactory Redis 연결 정보
+	 * @return 문자열 기반 키값을 지원하는 템플릿
+	 */
 	@Bean
 	public RedisTemplate<String, Object> redisTemplate(
 		RedisConnectionFactory connectionFactory
