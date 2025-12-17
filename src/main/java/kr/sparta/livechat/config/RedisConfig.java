f46223b0c39404c299a7ff858bfcdb3323d56c3a
sehyun kim <127721055+shkim6663@users.noreply.github.com>
2025-12-17 12:02:34 +0900

    feat: Redis 연결 및 RedisTemplate 설정
    - LettuceConnectionFactory 기반 Redis 연결
    - RedisTemplate 빈 생성 및 String 직렬화 설정
    
    Co-authored-by: [김세현]
    Refs:#3#4 (GIT/Issue Tracker ID)

---
+++ src/main/java/kr/sparta/livechat/config/RedisConfig.java
@@ -0,0 +1,48 @@
+package kr.sparta.livechat.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Redis 데이터베이스와의 연결하는 Spring 클래스입니다.
+ * yml 파일에서 Redis 정보를 읽어와 RedisConnectionFactory와 RedisTemplate을 생성하여
+ * Spring IoC 컨데티어네 등록합니다.
+ * RedisConfig.java
+ *
+ * @author kimsehyun
+ * @since 2025. 12. 16.
+ */
+@Configuration
+public class RedisConfig {
+
+	@Value("${spring.data.redis.host}")
+	private String redisHost;
+
+	@Value("${spring.data.redis.port}")
+	private int redisPort;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(redisHost, redisPort);
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(
+		RedisConnectionFactory connectionFactory
+	) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new StringRedisSerializer());
+		template.setHashValueSerializer(new StringRedisSerializer());
+
+		return template;
+	}
+}
